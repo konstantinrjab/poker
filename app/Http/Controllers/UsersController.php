5f2d7d8fe188f791dec97ce5d56df42b801be3b9
k.riabchenko <k.riabchenko@gamingtec.com>
2020-07-05 13:09:24 +0300

    removed TestDeal game logic. created register controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+
+class UsersController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+
+    }
+}

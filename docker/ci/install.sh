457c9ac10661d6530f26603d67561c1f7bf4fdee
k.riabchenko <k.riabchenko@gamingtec.com>
2020-10-23 12:45:35 +0300

    ci

---
+++ docker/ci/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+#apt-get update -yqq
+#apt-get install git -yqq
+
+apt-get install -y \
+    libzip-dev \
+    zip
+
+docker-php-ext-install zip
